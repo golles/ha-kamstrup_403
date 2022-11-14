00d32ab7c698aebc72faef6535b52288f16cfe04
Sander <developer@golles.nl>
2022-11-14 19:53:23 +0000

    Fix test

--- tests/const.py
+++ tests/const.py
@@ -1,6 +1,6 @@
 """Constants for kamstrup_403 tests."""
-from homeassistant.const import CONF_PORT, CONF_SCAN_INTERVAL
+from homeassistant.const import CONF_PORT, CONF_SCAN_INTERVAL, CONF_TIMEOUT
 
 # Mock config data to be used across multiple tests
 MOCK_CONFIG = {CONF_PORT: "/dev/ttyUSB0"}
-MOCK_UPDATE_CONFIG = {CONF_SCAN_INTERVAL: 120}
+MOCK_UPDATE_CONFIG = {CONF_SCAN_INTERVAL: 120, CONF_TIMEOUT: 1.0}
