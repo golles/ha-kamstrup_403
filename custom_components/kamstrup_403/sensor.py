d4aeaaa6eb0accb508ea91b0870d2fdeb2caff8c
Sander <developer@golles.nl>
2023-02-11 15:22:12 +0000

    Add support for date sensors

--- custom_components/kamstrup_403/sensor.py
+++ custom_components/kamstrup_403/sensor.py
@@ -1,4 +1,6 @@
 """Sensor platform for kamstrup_403."""
+from datetime import datetime
+
 from homeassistant.components.sensor import (
     DOMAIN as SENSOR_DOMAIN,
     SensorDeviceClass,
@@ -13,6 +15,7 @@ from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import StateType
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.util import dt
 
 from . import KamstrupUpdateCoordinator
 from .const import DEFAULT_NAME, DOMAIN
@@ -85,18 +88,6 @@ DESCRIPTIONS: list[SensorEntityDescription] = [
         entity_registry_enabled_default=False,
     ),
     SensorEntityDescription(
-        key="140",  # 0x008C
-        name="MinFlowDate_M",
-        icon="mdi:calendar",
-        entity_registry_enabled_default=False,
-    ),
-    SensorEntityDescription(
-        key="138",  # 0x008A
-        name="MaxFlowDate_M",
-        icon="mdi:calendar",
-        entity_registry_enabled_default=False,
-    ),
-    SensorEntityDescription(
         key="145",  # 0x0091
         name="MinPower_M",
         icon="mdi:flash",
@@ -129,18 +120,6 @@ DESCRIPTIONS: list[SensorEntityDescription] = [
         entity_registry_enabled_default=False,
     ),
     SensorEntityDescription(
-        key="144",  # 0x0090
-        name="MinPowerDate_M",
-        icon="mdi:calendar",
-        entity_registry_enabled_default=False,
-    ),
-    SensorEntityDescription(
-        key="142",  # 0x008E
-        name="MaxPowerDate_M",
-        icon="mdi:calendar",
-        entity_registry_enabled_default=False,
-    ),
-    SensorEntityDescription(
         key="126",  # 0x007E
         name="MinFlow_Y",
         icon="mdi:water",
@@ -152,18 +131,6 @@ DESCRIPTIONS: list[SensorEntityDescription] = [
         name="MaxFlow_Y",
         icon="mdi:water",
         state_class=SensorStateClass.MEASUREMENT,
-        entity_registry_enabled_default=False,
-    ),
-    SensorEntityDescription(
-        key="125",  # 0x007D
-        name="MinFlowDate_Y",
-        icon="mdi:calendar",
-        entity_registry_enabled_default=False,
-    ),
-    SensorEntityDescription(
-        key="123",  # 0x007B
-        name="MaxFlowDate_Y",
-        icon="mdi:calendar",
         entity_registry_enabled_default=False,
     ),
     SensorEntityDescription(
@@ -196,18 +163,6 @@ DESCRIPTIONS: list[SensorEntityDescription] = [
         icon="mdi:thermometer",
         device_class=SensorDeviceClass.TEMPERATURE,
         state_class=SensorStateClass.MEASUREMENT,
-        entity_registry_enabled_default=False,
-    ),
-    SensorEntityDescription(
-        key="129",  # 0x0081
-        name="MinPowerDate_Y",
-        icon="mdi:calendar",
-        entity_registry_enabled_default=False,
-    ),
-    SensorEntityDescription(
-        key="127",  # 0x007F
-        name="MaxPowerDate_Y",
-        icon="mdi:calendar",
         entity_registry_enabled_default=False,
     ),
     SensorEntityDescription(
@@ -252,6 +207,66 @@ DESCRIPTIONS: list[SensorEntityDescription] = [
 ]
 
 
+DATE_DESCRIPTIONS: list[SensorEntityDescription] = [
+    SensorEntityDescription(
+        key="140",  # 0x008C
+        name="MinFlowDate_M",
+        icon="mdi:calendar",
+        device_class=SensorDeviceClass.TIMESTAMP,
+        entity_registry_enabled_default=False,
+    ),
+    SensorEntityDescription(
+        key="138",  # 0x008A
+        name="MaxFlowDate_M",
+        icon="mdi:calendar",
+        device_class=SensorDeviceClass.TIMESTAMP,
+        entity_registry_enabled_default=False,
+    ),
+    SensorEntityDescription(
+        key="144",  # 0x0090
+        name="MinPowerDate_M",
+        icon="mdi:calendar",
+        device_class=SensorDeviceClass.TIMESTAMP,
+        entity_registry_enabled_default=False,
+    ),
+    SensorEntityDescription(
+        key="142",  # 0x008E
+        name="MaxPowerDate_M",
+        icon="mdi:calendar",
+        device_class=SensorDeviceClass.TIMESTAMP,
+        entity_registry_enabled_default=False,
+    ),
+    SensorEntityDescription(
+        key="125",  # 0x007D
+        name="MinFlowDate_Y",
+        icon="mdi:calendar",
+        device_class=SensorDeviceClass.TIMESTAMP,
+        entity_registry_enabled_default=False,
+    ),
+    SensorEntityDescription(
+        key="123",  # 0x007B
+        name="MaxFlowDate_Y",
+        icon="mdi:calendar",
+        device_class=SensorDeviceClass.TIMESTAMP,
+        entity_registry_enabled_default=False,
+    ),
+    SensorEntityDescription(
+        key="129",  # 0x0081
+        name="MinPowerDate_Y",
+        icon="mdi:calendar",
+        device_class=SensorDeviceClass.TIMESTAMP,
+        entity_registry_enabled_default=False,
+    ),
+    SensorEntityDescription(
+        key="127",  # 0x007F
+        name="MaxPowerDate_Y",
+        icon="mdi:calendar",
+        device_class=SensorDeviceClass.TIMESTAMP,
+        entity_registry_enabled_default=False,
+    ),
+]
+
+
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
@@ -262,13 +277,23 @@ async def async_setup_entry(
 
     entities: list[KamstrupSensor] = []
 
-    # Add all meter sensors described above.
+    # Add all meter sensors.
     for description in DESCRIPTIONS:
         entities.append(
             KamstrupMeterSensor(
                 coordinator=coordinator,
                 entry_id=entry.entry_id,
                 description=description,
+            )
+        )
+
+    # Add all date sensors.
+    for date_description in DATE_DESCRIPTIONS:
+        entities.append(
+            KamstrupDateSensor(
+                coordinator=coordinator,
+                entry_id=entry.entry_id,
+                description=date_description,
             )
         )
 
@@ -343,6 +368,36 @@ class KamstrupMeterSensor(KamstrupSensor):
             return self.coordinator.data[self.int_key].get("unit", None)
 
         return None
+
+
+class KamstrupDateSensor(KamstrupMeterSensor):
+    """Defines a Kamstrup date sensor."""
+
+    @property
+    def native_value(self) -> datetime | None:
+        """Return the state of the sensor."""
+        value = super().native_value
+        if value is not None:
+            return self.to_datetime(value)
+
+        return None
+
+    @property
+    def native_unit_of_measurement(self) -> None:
+        """Return the unit of measurement of the sensor, if any."""
+        return None
+
+    @classmethod
+    def to_datetime(cls, value: float) -> datetime | None:
+        """
+        Convert a meter value to a datetime object.
+        The value from the meter could be "230101.0" (yymmdd as float).
+        The returned datetime is in UTC.
+        """
+        string = str(
+            int(value)
+        )  # Removes the decimal and convert to string for strptime.
+        return dt.as_local(datetime.strptime(string, "%y%m%d"))
 
 
 class KamstrupGasSensor(KamstrupSensor):
