a2e1ab6fed916f8f9d624f211f04417255b76e8a
Sander <developer@golles.nl>
2023-03-05 12:40:44 +0000

    Release version `2.4.0`

--- custom_components/kamstrup_403/const.py
+++ custom_components/kamstrup_403/const.py
@@ -5,7 +5,7 @@ from typing import Final
 # Base component constants
 NAME: Final = "Kamstrup 403"
 DOMAIN: Final = "kamstrup_403"
-VERSION: Final = "2.3.0"
+VERSION: Final = "2.4.0"
 MODEL: Final = "403"
 MANUFACTURER: Final = "Kamstrup"
 ATTRIBUTION: Final = "Data provided by Kamstrup 403 meter"
