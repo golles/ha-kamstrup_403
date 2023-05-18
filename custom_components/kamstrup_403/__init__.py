aa4de2f45cbdd897210dbdf0582678f8c6e268e9
Sander <developer@golles.nl>
2023-05-18 15:02:14 +0000

    Fix formatting

--- custom_components/kamstrup_403/__init__.py
+++ custom_components/kamstrup_403/__init__.py
@@ -68,7 +68,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     await coordinator.async_config_entry_first_refresh()
-    
+
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
 
     return True
